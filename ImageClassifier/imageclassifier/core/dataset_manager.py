d0c222e02857c65757ee7b3b031e93a071ecf100
Natalie Sova <natsova@protonmail.com>
2025-12-02 14:42:22 +1100

    Update dataset_manager.py

--- ImageClassifier/imageclassifier/core/dataset_manager.py
+++ ImageClassifier/imageclassifier/core/dataset_manager.py
@@ -1,1 +1,11 @@
+class DatasetManager:
+    def __init__(self, config: Config):
+        self.config = config
 
+    def setup(self):
+        create_folders_for_categories(self.config)
+        download_images(self.config)
+        select_img_for_deletion(self.config)
+        refill_categories(self.config)
+        display_images(self.config)
+        create_dataloader(self.config)
