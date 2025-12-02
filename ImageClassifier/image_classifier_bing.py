aaa1921e0d93d2ba54a4151cf42b51cb0a5d007d
Natalie Sova <natsova@protonmail.com>
2025-12-02 14:31:06 +1100

    Update image_classifier_bing.py

--- ImageClassifier/image_classifier_bing.py
+++ ImageClassifier/image_classifier_bing.py
@@ -358,7 +358,7 @@ display_images(config)
 
 check_datasets(config)
 
-# Create dataloader
+# # Create dataloader
 dls = create_dataloader(config)
 
 # Check categories in validation set
@@ -377,7 +377,7 @@ learn.fine_tune(10)
 interp = ClassificationInterpretation.from_learner(learn)
 interp.plot_confusion_matrix()
 
-# Apply model
+# # Apply model
 
 # Process an image through the same training pipeline
 url = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcontent.lyka.com.au%2Ff%2F1016262%2F4288x2848%2F6fef92978a%2Fspoodle-puppy.jpeg%2Fm%2F1280x0%2Ffilters%3Aformat(webp)&f=1&nofb=1&ipt=aff6f2e8af6a16d4ca4e06da3fb681d12344283830ffd571b25039e60182e01c"
